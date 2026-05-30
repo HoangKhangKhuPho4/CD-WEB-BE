eb9733133af2fcf3a6f76017f8685c7337f8280f
HoangKhangKhuPho4 <22130116@st.hcmuaf.edu.vn>
2026-05-30 11:57:52 +0700

    chore: remove debug artifacts, IDE config and local DB dump from repo

--- src/main/java/com/cdweb/be/HashGen.java
+++
@@ -1,9 +0,0 @@
-package com.cdweb.be;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class HashGen {
-  public static void main(String[] args) {
-    System.out.println("HASH=" + new BCryptPasswordEncoder().encode("admin123"));
-  }
-}
